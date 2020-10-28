9de200e04c1657754cc1a59b250400ae9057e66d
Zevell <zevalgaming@gmail.com>
2020-10-29 04:26:22 +1000

    Select response if keywords match
    Randomly select from the list of responses if the input from the user is found in the keywords.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import json
+import random
 groupings = None
 
 with open('groupings.json') as jsonFile:
   groupings = json.load(jsonFile)
+
+while True: 
+  userInput = input("")
+  
+  for group in groupings['groupings']:
+    for element in groupings['groupings']['keywords']:
+      if userInput.contains(element):
+        print(random.choice(groupings['groupings']['keywords']))
